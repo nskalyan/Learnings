b8d461ea458808cf1e72af7de57a8f5cdeb2ba62
nskalyan <118542547+nskalyan@users.noreply.github.com>
2025-01-11 02:21:42 +0000

    Sync LeetCode submission Runtime - 17 ms (88.12%), Memory - 59.2 MB (99.94%)

--- solutions/0217-contains-duplicate/solution.java
+++ solutions/0217-contains-duplicate/solution.java
@@ -11,13 +11,5 @@ class Solution {
             return true;
         }  
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int[] nums = new int[size];
-        for(int i=0;i<size;i++){
-            nums[i]=sc.nextInt();
-        }
-        sc.close();
-    }
+
 }

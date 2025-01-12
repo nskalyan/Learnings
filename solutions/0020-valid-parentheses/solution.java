af1f6583d2d37f546b784f22bb31e4326a6efb50
nskalyan <118542547+nskalyan@users.noreply.github.com>
2025-01-12 03:17:12 +0000

    Sync LeetCode submission Runtime - 2 ms (97.97%), Memory - 42 MB (99.96%)

---
+++ solutions/0020-valid-parentheses/solution.java
@@ -0,0 +1,27 @@
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            } else {
+                if (st.isEmpty()) {
+                    return false; 
+                }
+                
+                if ((st.peek() == '(' && c == ')') || 
+                    (st.peek() == '[' && c == ']') || 
+                    (st.peek() == '{' && c == '}')) {
+                    st.pop(); 
+                } else {
+                    return false; 
+                }
+            }
+        }
+        
+        return st.isEmpty();
+    }
+}
+

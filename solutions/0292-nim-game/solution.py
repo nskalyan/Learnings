18ab9323f7cbaca250f34e480d7762a6416e6948
nskalyan <118542547+nskalyan@users.noreply.github.com>
2026-07-28 10:56:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (82.28%)

---
+++ solutions/0292-nim-game/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        if n<=3:
+            return True
+        elif n%4!=0:
+            return True
+        else:
+            return False
+        
+        
